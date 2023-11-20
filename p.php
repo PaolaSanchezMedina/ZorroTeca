539ef9a3103cfb3dc2b3e8143dff4898860f0363
FidelXXl <102196459+FidelXXl@users.noreply.github.com>
2023-11-20 17:13:13 -0600

    Creacion del forato de la presentacion

--- p.php
+++ p.php
@@ -18,3 +18,81 @@
 <body >
 <div id="nav-placeholder">
         <nav class="navbar navbar-expand-lg bg-body-orange">
+
+            <div class="container-fluid">
+                <a class="navbar-logo" href="#"><img class="img-logo" src="img/zorro.png"
+                        alt=""></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                        <li class="nav-item">
+                            <a class="nav-link fs-5 fw-bold" href="libros.php">ZorroTeca</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul class="navbar-nav">
+                        <li class="nav-item li-bg-cierre">
+                            <a class="nav-link active" href="mostrarlib.php">Ir a la pagina</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <!--CUERPO DE PÁGINA-->
+
+    <div class="row">  
+    <div clas=" mt-12">
+        <div class="presentacion1" >
+            <h1 style="text-align: center" >Proyecto biblioteca digital </h1>
+                <div class="presentacion2"  style="height: 200px">
+                    <h2>Sección 1</h2>
+                    <p>Contenido de la sección 1...</p>
+                </div>
+    
+    <div class="row">
+        <div class="col-md-6 ">
+        <div class="presentacion2" style="height: 200px">
+        <h2>Sección 2</h2>
+        <p>Contenido de la sección 1...</p>
+            <!-- Contenido de la primera columna -->
+        </div>
+        </div>
+        <div class="col-md-6">
+        <div class="presentacion2"  style="height: 200px">
+        <h2>Sección 3</h2>
+        <p>Contenido de la sección 1...</p>
+            <!-- Contenido de la primera columna -->
+        </div>
+        </div>
+        <div class="col-md-9 ">
+        <div class="presentacion2" style="height: 200px">
+        <h2>Sección 4</h2>
+        <p>Contenido de la sección 1...</p>
+            <!-- Contenido de la primera columna -->
+        </div>
+        </div>
+        <div class="col-md-3 ">
+        <div class="presentacion2" style="height: 200px">
+        <h2>Sección 5</h2>
+        <p>Contenido de la sección 1...</p>
+            <!-- Contenido de la primera columna -->
+        </div>
+        </div>
+    </div>
+    </div>
+                
+</div>
+
+    </div>
+
+        </div>
+
+</body>
