83b83c831f9a4205e756b817188088d83477268c
FidelXXl <102196459+FidelXXl@users.noreply.github.com>
2023-11-20 14:04:28 -0600

    Create p.php
    Presentación

---
+++ p.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Hoja de estilos css-->
+    <link rel="stylesheet" href="css/style.css">
+    <!--Icono de la página-->
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <!--Bootstrap 5-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>ZorroTeca</title>
+</head>
+
+<body >
+<div id="nav-placeholder">
+        <nav class="navbar navbar-expand-lg bg-body-orange">
